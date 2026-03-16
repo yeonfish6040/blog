53a218f6a622de2f77b6c98bd55f3913924fd9d2
Yeonjun Lee <yeonfish6040@gmail.com>
2026-03-16 10:47:48 +0900

    url

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.lyj.kr/",
+  site: "https://yeonfish.dev/",
   base: "/",
   trailingSlash: "always",
   integrations: [
